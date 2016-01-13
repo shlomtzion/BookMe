e070002cb79c3e06c95cfeff89a67987b73d99eb
shlomtzion <shlomtzyon@gmail.com>
2016-01-13 10:16:46 +0200

    עף בdelete

--- app/src/main/java/com/example/naamashlomtzion/bookme/DeleteBook.java
+++ app/src/main/java/com/example/naamashlomtzion/bookme/DeleteBook.java
@@ -9,10 +9,10 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.Toast;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import Model.Backend.Backend;
@@ -52,6 +52,7 @@ public class DeleteBook extends Fragment/* implements AdapterView.OnItemClickLis
         // Inflate the layout for this fragment
         //super.onCreate(savedInstanceState);
         View rootView=inflater.inflate(R.layout.fragment_delete_book,container,false);
+        Button button = (Button)rootView.findViewById(R.id.delete_book);
         Spinner spinner=(Spinner)rootView.findViewById(R.id.spinner);
         Bundle bundle = this.getArguments();
         if (bundle != null) {
@@ -59,19 +60,56 @@ public class DeleteBook extends Fragment/* implements AdapterView.OnItemClickLis
         }
 
 
-       // spinner.setOnItemClickListener(this);??
-        List<String> namebook=new ArrayList<String>();//nameOfBook
+
+
+        //List<String> namebook=new ArrayList<String>();//nameOfBook
+        String[] nameBooksStrings;
+        nameBooksStrings = new String[1];
+        nameBooksStrings[0]="הרשימה ריקה";
         try {
-            namebook=backend.nameBook();
+
+            List<String> namebook=backend.nameBook();
+            nameBooksStrings = new String[namebook.size()];
+            int i=0;
+            for(String s:namebook)
+            {
+                nameBooksStrings[i]=s;
+                i++;
+            }
+
+
         }
         catch (Exception e) {
             e.printStackTrace();
         }
-            ArrayAdapter<CharSequence>adapter;
-       // adapter=ArrayAdapter.createFromResource(getActivity(),R.array.nav_drawer_items, android.R.layout.simple_spinner_item);
+        ArrayAdapter<String>adapter;
+        //adapter=ArrayAdapter.createFromResource(getContext(),nameBooksStrings,android.R.layout.simple_spinner_item);
+        adapter= new ArrayAdapter<String>(getActivity(),R.layout.support_simple_spinner_dropdown_item,nameBooksStrings);
             //adapter = new ArrayAdapter<String>(this,R.layout.row_name_book, namebook);
-          //  adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-           // spinner.setAdapter(adapter);
+        //adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String item = parent.getItemAtPosition(position).toString();
+                String idBook = item.substring(item.indexOf("id:"));
+                //Toast.makeText(getContext(), "הספר שנבחר:" + item, Toast.LENGTH_LONG).show();
+                try {
+                    //backend.deleteBook(Long.parseLong(idBook),idProvider, Privileging.PROVIDER);
+                    //Toast.makeText(parent.getContext(),,Toast.LENGTH_LONG).show();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                Toast.makeText(parent.getContext(), "הספר נמחק", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
 
 
          return rootView;
